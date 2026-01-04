e257034e2d78e93158f92c7fdc710d0929a4ab5a
Jimyeong Kim <kjm021221@icloud.com>
2026-01-04 11:45:38 +0900

    🎉Begin a Project

---
+++ src/main/java/com/project/toosung_back/TooSungBackApplication.java
@@ -0,0 +1,13 @@
+package com.project.toosung_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TooSungBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TooSungBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/toosung_back/TooSungBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.toosung_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TooSungBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
