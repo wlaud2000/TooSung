971004286cb685f8427c7eb1dc1131b797f0be17
Jimyeong Kim <kjm021221@icloud.com>
2026-03-29 12:00:06 +0900

    ✨feat: [Disclosure] 에러코드 / DTO / Converter 작성

---
+++ src/main/java/com/project/toosung_back/domain/disclosure/exception/DisclosureErrorCode.java
@@ -0,0 +1,16 @@
+package com.project.toosung_back.domain.disclosure.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public enum DisclosureErrorCode {
+
+    DISCLOSURE_NOT_FOUND(HttpStatus.NOT_FOUND, "DISCLOSURE404_1", "존재하지 않는 공시입니다");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
