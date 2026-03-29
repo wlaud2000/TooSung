9d399f1d42c1c4c08d38523b031f70f4461d8c0a
Jimyeong-Kim <86175262+wlaud2000@users.noreply.github.com>
2026-03-29 12:16:00 +0900

    🐛fix: 어노테이션 import 경로 수정

--- src/main/java/com/project/toosung_back/domain/disclosure/repository/DisclosureRepository.java
+++ src/main/java/com/project/toosung_back/domain/disclosure/repository/DisclosureRepository.java
@@ -1,7 +1,7 @@
 package com.project.toosung_back.domain.disclosure.repository;
 
 import com.project.toosung_back.domain.disclosure.entity.Disclosure;
-import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
